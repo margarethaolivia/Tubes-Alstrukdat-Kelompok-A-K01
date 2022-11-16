139249821d65933965a7b0795b6702b4a1c62af3
haidar <89732397+haidarhamda@users.noreply.github.com>
2022-11-16 22:16:38 +0700

    display day in printtime

--- ADT/Time/time.c
+++ ADT/Time/time.c
@@ -21,7 +21,7 @@ void prevMin(time *t){
     advTime(t,0,0,-1);
 }
 void printTime(time t){
-    printf("%d.%d\n",hour(t),minute(t));
+    printf("%d.%d.%d\n",day(t),hour(t),minute(t));
 }
 boolean isTimeValid(time t){
     return(day(t)>=0 && hour(t)>=0 && hour(t) < 24  && minute(t) >= 0 && minute(t) < 60);
