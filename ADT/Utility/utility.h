e4983631e62dbc1fa96968ec6f66083c59294dfe
haidar <89732397+haidarhamda@users.noreply.github.com>
2022-11-16 22:11:46 +0700

    Update utility.h

--- ADT/Utility/utility.h
+++ ADT/Utility/utility.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "../Boolean/boolean.h"
+#include "../Mesin_Kata/mesinkata.h"
 
 boolean strcmp(const char* s1, const char* s2);
 //komparasi string 1 dengan string lainnya, bisa untuk komparasi buffer kata dengan
 //strin lain atau buffer kata lain
+
+int stoi(Kata s);
+
+Kata concat(const char* s1, const char* s2);
+
